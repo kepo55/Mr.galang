3051ce653b7350b092ad0d1b09f63078765b6b6a
kepo55 <64832211+kepo55@users.noreply.github.com>
2020-05-08 14:51:51 +0700

    Update Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -11,6 +11,7 @@ echo "\033[0;32mpilih menu Toolsnya"
 echo "\033[1;33m[1]hack fecebok"
 echo "\033[1;31m[2]spam JDID"
 echo "\033[0;32m[3]Hack Pulsa"
+echo "'\e[1;34m'[4]install bahan"
 read -p "[?] pilih : " pil;
 # sabar gan
 if [ $pil = 1 ]
@@ -26,8 +27,23 @@ git clone https://github.com/kepo55/Call
 cd Call
 php spamcall.php
 fi
+# sabar gan
 if [ $pil = 3 ]
 then
 git clone https://github.com/kepo55/Hack-pulsa
 cd Hack-pulsa
 sh Pulsa.sh
+fi
+if [ $pil = 4 ]
+then
+pkg update && pkg upgrade
+pkg install nano
+pkg install python
+pkg install git
+pkg install python2
+pip2 install requests mechanize
+pip install requests mechanize
+pkg install php
+
+
+
