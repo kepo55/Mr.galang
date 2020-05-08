ac20db80947335c4e2a286e00cd0f76f432c3991
kepo55 <64832211+kepo55@users.noreply.github.com>
2020-05-08 14:24:56 +0700

    Update Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -14,8 +14,9 @@ echo "\033[0;32m[3]Hack Pulsa"
 echo "\033[1;33m[4]Install Bahan"
 read -p "[?] pilih : " pil:
 # sabar gan
-fi [ $pil = 1 ]
+if [ $pil = 1 ]
 then
 git clone https://github.com/kepo55/jembutz
 cd jembutz
 python2 jembutz
+fi
