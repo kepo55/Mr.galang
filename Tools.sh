8f37e4bc63ec1c5a7fcabf4dfdda5aaba9d9b3e2
kepo55 <64832211+kepo55@users.noreply.github.com>
2020-05-08 11:36:21 +0700

    Update Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -5,3 +5,11 @@ echo "\033[0;32mAuthor =Mr.G4L4N9"
 echo "\033[1;33myoutube =JKT Your Gaming"
 echo "\033[1;31mgithub =https://github.com/kepo55"
 echo "\033[1;33m----------------------------------"
+echo
+echo "\033[0;32mpilih menu Toolsnya"
+echo "\033[1;33m[1]hack fecebok"
+echo "\033[1;31m[2]spam JDID"
+echo "\033[0;32m[3]Hack Pulsa"
+echo "\033[1;33m[4]Install Bahan"
+read -p "\033[1;31m[?] pilih : " pil:
+# sabar gan
