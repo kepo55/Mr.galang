9d16f1351c0bc64d072710fb8491d9ce552acea2
kepo55 <64832211+kepo55@users.noreply.github.com>
2020-05-08 11:21:56 +0700

    Create Tools.sh

---
+++ Tools.sh
@@ -0,0 +1,7 @@
+#Authornya jangan di ganti hargi yg sudah buatnya
+clear
+echo "\033[1;33m----------------------------------"
+echo "\033[1;0mAuthor =Mr.G4L4N9"
+echo "\033[1;30myoutube =JKT Your Gaming"
+echo "\033[1;31mgithub =https://github.com/kepo55"
+echo "\033[1;33m----------------------------------"
